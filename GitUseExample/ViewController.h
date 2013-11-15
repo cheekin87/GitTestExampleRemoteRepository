56c4530cee5f6190e81671cff2dac91c633cdc69
cheekin <cheekin@MacBook.local>
2013-11-15 11:28:56 +0800

    Initial Commit

---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by cheekin on 11/15/13.
+//  Copyright (c) 2013 cheekin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitUseExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitUseExample
+//
+//  Created by cheekin on 11/15/13.
+//  Copyright (c) 2013 cheekin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
